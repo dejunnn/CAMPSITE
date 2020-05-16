7e51bcdcd8bfa6097a5100650308142966ee7d2f
dejunnn <djplanners@hotmail.com>
2020-05-16 18:06:47 +0800

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+// Hamburger menu animation
+function toggleMenuAnimation(x) {
+  x.classList.toggle("change");
+}
+
+function toggleMenu() {
+  var element = document.getElementById("dropdown");
+  element.classList.toggle("change");
+}
